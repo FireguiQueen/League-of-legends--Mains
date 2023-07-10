696d79a209e122e5656e965124726192c294fce1
Guilherme <Fireguiquenbr@gmail.com>
2023-07-09 21:34:36 -0300

    update

--- test.js
+++ test.js
@@ -1,15 +1,13 @@
 // All my mains will be here.
-const ChampList = [];
+const champList = [];
 
 // Creating a new champion.
 function creatChamp(champion, mastery) {
-  ChampList.push({
+  champList.push({
     champion,
     mastery
   });
 }
-
-
 
 // My mains. Arguments: Champion-name & champion-mastery
 const neeko = creatChamp('neeko', 1000);
@@ -17,5 +15,26 @@ const kassadin = creatChamp('kassadin', 1000);
 const veigar = creatChamp('veigar', 1000);
 const ekko = creatChamp('ekko', 1000);
 
+
+function creatCard(champion){
+    const {champion: name, mastery } = champion
+    return `
+        <div id="card">
+            <section id="front" class="face">
+                <h1> ${name} </h1>
+            </section>
+
+            <section id="back" class="face">
+                <h1> ${mastery} 
+            </section>
+        </div>
+    `
+}
+
+
+const bodyPage = document.querySelector('body');
+for(let i = 0; i < champList.length; i++){
+    bodyPage.innerHTML += creatCard(champList[i]); 
+}
 
 
